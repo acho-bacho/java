1b1dc8f10301f5d4a9644b7ea372804aa324d0c5
akit <angel.kitov@softwareag.com>
2019-06-03 13:49:57 +0000

    first commit

---
+++ collections/src/collections/set/HashSet.java
@@ -0,0 +1,7 @@
+package collections.set;
+
+public class HashSet {
+    public static void main(String[] args) {
+        System.out.println("HashSet");
+    }
+}
---
+++ collections/src/com/kitov/angel/Main.java
@@ -0,0 +1,8 @@
+package com.kitov.angel;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
