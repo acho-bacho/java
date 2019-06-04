ebffa64191e2dea44a5ffbcbe0c7c1d5ef1a82f2
acho-bacho <acho.amst@gmail.com>
2019-06-04 20:20:52 +0000

    commit from home

--- sandbox/src/collections/set/LinkedHashSetExample.java
+++ sandbox/src/collections/set/LinkedHashSetExample.java
@@ -1,17 +1,20 @@
 package collections.set;
+
 import java.util.*;
-public class HashSetExample {
+
+public class LinHashSetExample {
+
+
     public static void main(String args[]) {
-        //Creating HashSet and adding elements  
-        HashSet<String> set = new HashSet<String>();
+        LinkedHashSet<String> set = new LinkedHashSet<String>();
         set.add("Ravi");
         set.add("Vijay");
         set.add("Ravi");
         set.add("Ajay");
-        //Traversing elements  
         Iterator<String> itr = set.iterator();
         while (itr.hasNext()) {
             System.out.println(itr.next());
         }
     }
+
 }
