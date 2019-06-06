c73d0f81b6dde9eb0a3f0fb6d719902ce946d82f
akit <angel.kitov@softwareag.com>
2019-06-06 15:02:52 +0000

    added some algorithm problems

--- sandbox/src/algorithms/others/PascalTriangle.java
+++ sandbox/src/algorithms/others/PascalTriangle.java
@@ -1,13 +1,19 @@
 package algorithms.others;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class PascalTriangle {
     public static void main(String[] args) {
 
-        ArrayList<ArrayList<Integer>> result = build(3);
-        //ArrayList<ArrayList<Integer> > result = {{1,3}, {4,5}};
+        int[][] resultArray = buildArray(8);
+        for (int i = 0; i < resultArray.length; i++) {
+            for (int j = 0; j < resultArray.length; j++) {
+                System.out.print(resultArray[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        ArrayList<ArrayList<Integer>> result = build(8);
 
         for (ArrayList<Integer> al :
                 result) {
@@ -15,53 +21,59 @@ public class PascalTriangle {
         }
     }
 
+    static int[][] buildArray(int depth) {
+
+        int[][] result = new int[depth][depth];
+
+        for (int h = 0; h < depth; h++) {
+
+            int[] line = result[h];
+
+            for (int x = 0; x < h + 1; x++) {
+
+                if (x == 0 || x == h) {
+                    line[x] = 1;
+                    continue;
+                }
+                if (h > 0) {
+                    int[] prevLine = result[h - 1];
+                    line[x] = prevLine[x] + prevLine[x - 1];
+                }
+            }
+        }
+        return result;
+    }
+
+
+    // same but with arrayList
     static ArrayList<ArrayList<Integer>> build(int depth) {
-
-
+        // populate the arrayList
         ArrayList<ArrayList<Integer>> result = new ArrayList<ArrayList<Integer>>(depth);
         for (int i = 0; i < depth; i++) {
             ArrayList<Integer> line = new ArrayList<Integer>(depth);
-            for (int j = 0; j < depth; j++)
+            for (int j = 0; j < depth; j++) {
                 line.add(null);
+            }
             result.add(line);
         }
 
-        System.out.println("size " + result.size());
-
-
         for (int h = 0; h < depth; h++) {
-            ArrayList<Integer> line = result.get(h);
-            ArrayList<Integer> prevLine = result.get(h - 1);
-            System.out.println("line is " + line);
 
-            for (int x = 0; x < depth; x++) {
-                System.out.println("x " + x);
-                System.out.println("h " + h);
+            ArrayList<Integer> line = result.get(h);
+
+            for (int x = 0; x < h + 1; x++) {
+
                 if (x == 0 || x == h) {
                     line.set(x, 1);
-                    System.out.println("set value ");
                     continue;
                 }
-
-                //line.set(x, 88);
+                if (h > 0) {
+                    ArrayList<Integer> prevLine = result.get(h - 1);
+                    line.set(x, prevLine.get(x) + prevLine.get(x - 1));
+                }
             }
         }
-/*
-        for (int h = 0; h < depth; h++) {
-
-            ArrayList<Integer> line = result.get(h);
-            System.out.println("get line ");
-            System.out.println("line is " + line);
-            System.out.println("line size " + line.size());
-            for (int i = 0; i < line.size(); i++) {
-                //if (i == 0 || i == h - 1) {
-                    line.set(i, 1);
-                System.out.println("set value ");
-                 //   continue;
-               // }
-            }
-        }
-*/
         return result;
     }
+
 }
