4525035b4206c9070e3531d42c21bcada197301a
akit <angel.kitov@softwareag.com>
2019-06-12 15:17:55 +0000

    bubble

---
+++ sandbox/src/algorithms/sortingAndSearching/BubbleSort.java
@@ -0,0 +1,34 @@
+package algorithms.sortingAndSearching;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] input = {6, 4, 3, 7, 1, 12, 9, 8, 9, 10};
+
+
+        System.out.println("array " + Arrays.toString(input));
+        sort(input);
+        System.out.println("array " + Arrays.toString(input));
+    }
+
+    static void sort(int[] arr) {
+        boolean madeSwaps = false;
+        int tmp = 0;
+        do {
+            madeSwaps = false;
+            for (int i = 0; i < arr.length-1; i++) {
+                if(arr[i]>arr[i+1]){
+                    tmp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = tmp;
+                    madeSwaps = true;
+                }
+            }
+        } while (madeSwaps);
+
+
+        System.out.println("done");
+    }
+}
