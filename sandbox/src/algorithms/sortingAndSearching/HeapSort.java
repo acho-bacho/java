72c92b5c4d9fbb96bbbfc61985a1cc6f0c74345d
akit <angel.kitov@softwareag.com>
2019-06-13 15:04:43 +0000

    heap sort

--- sandbox/src/algorithms/sortingAndSearching/HeapSort.java
+++ sandbox/src/algorithms/sortingAndSearching/HeapSort.java
@@ -7,7 +7,8 @@ import java.util.Arrays;
 public class HeapSort {
 
     public static void main(String[] args) {
-        int[] input = {6, 4, 3, 7, 1, 12, 9, 8, 9, 13};
+        //int[] input = {6, 4, 3, 7, 1, 12, 9, 8, 9, 13};
+        int[] input = {1,2,10,5};
 
         System.out.println("array " + Arrays.toString(input));
         sort(input);
@@ -19,9 +20,9 @@ public class HeapSort {
         int size = arr.length;
 
         // Build heap (rearrange array)
-        // why size / 2 - 1
-        for (int i = size / 2 - 1; i >= 0; i--) {
-            heapify(arr, size, i);
+        // (size / 2 - 1) is  the start root and we go backwards
+        for (int rootPos = size / 2 - 1; rootPos >= 0; rootPos--) {
+            heapify(arr, rootPos, size);
         }
 
         // after heapify the largest element is at pos 0
@@ -29,43 +30,49 @@ public class HeapSort {
 
         // One by one extract an element from heap
         // and put it to ordered list at the end
-        for (int newSize=size-1; newSize>=0; newSize--)
+        // 'end' marks the end of the heap, after that starts the sorted part
+        for (int end = (size-1); end>=0; end--)
         {
+            //after heapify arr[0] is largest, so put it to end
             // Move current root to end
-            int temp = arr[0];
-            arr[0] = arr[newSize];
-            arr[newSize] = temp;
+            int largest = arr[0];
+            arr[0] = arr[end];
+            arr[end] = largest;
 
-            // call max heapify on the reduced heap
-            heapify(arr, newSize, 0);
+            // call max heapify on the reduced heap (reduced as end is --)
+            heapify(arr, 0, end);
         }
     }
 
     // To heapify a subtree rooted with node i which is
-    // an index in arr[]. n is size of heap
-    static void heapify(int arr[], int size, int rootPos)
+    // an index in arr[].
+    static void heapify(int[] arr, int root, int size)
     {
-        int largest = rootPos; // Initialize largest as root
-        int l = 2*rootPos + 1; // left = 2*i + 1
-        int r = 2*rootPos + 2; // right = 2*i + 2
+        // pos of largest element
+        int largest = root; // Initialize largest as root
+        int l = 2*root + 1; // left = 2*i + 1
+        int r = 2*root + 2; // right = 2*i + 2
 
         // If left child is larger than root
-        if (l < size && arr[l] > arr[largest])
+        if (l < size && arr[l] > arr[largest]) {
             largest = l;
+        }
 
         // If right child is larger than largest so far
-        if (r < size && arr[r] > arr[largest])
+        if (r < size && arr[r] > arr[largest]) {
             largest = r;
+        }
 
-        // If largest is not root
-        if (largest != rootPos)
+        // If largest is not root -> swaping -> new call of heapify()
+        if (largest != root)
         {
-            int swap = arr[rootPos];
-            arr[rootPos] = arr[largest];
-            arr[largest] = swap;
-
+            //swap so that root is bigger than children
+            int tmp = arr[root];
+            arr[root] = arr[largest];
+            arr[largest] = tmp;
+            System.out.println("** internal swap  = " + Arrays.toString(arr));
             // Recursively heapify the affected sub-tree
-            heapify(arr, size, largest);
+            heapify(arr, largest, size);
         }
     }
 }
